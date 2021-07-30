0f74dbeca5dec212706836857b95dcea3f05ff8f
mltomlins0n <martin070891@gmail.com>
2021-07-30 16:50:49 +0100

    Add IntegerChecker class member to flow class

--- src/main/java/CannonGameFlow.java
+++ src/main/java/CannonGameFlow.java
@@ -2,16 +2,19 @@ public class CannonGameFlow implements ICannonGameFlow {
 
     private ITargetGenerator _targetGenerator;
     private IShot _shot;
+    private IIntegerChecker _integerChecker;
 
-    public CannonGameFlow(ITargetGenerator targetGenerator, IShot shot) {
+    public CannonGameFlow(ITargetGenerator targetGenerator, IShot shot, IIntegerChecker integerChecker) {
         this._targetGenerator = targetGenerator;
         this._shot = shot;
+        this._integerChecker = integerChecker;
     }
 
     public String flow(String angle, String velocity) {
 
         _targetGenerator.getTarget();
         _shot.calculateShot();
+        _integerChecker.isInt(angle, velocity);
 
         return "";
     }
