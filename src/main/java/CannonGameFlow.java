e65bf7dbf393ceb3f3bfd0f2430a92a3698c3071
mltomlins0n <martin070891@gmail.com>
2021-07-30 18:29:56 +0100

    Add logic to CannonGameFlow class

--- src/main/java/CannonGameFlow.java
+++ src/main/java/CannonGameFlow.java
@@ -11,12 +11,12 @@ public class CannonGameFlow implements ICannonGameFlow {
     }
 
     public String flow(String angle, String velocity) {
-
         _targetGenerator.getTarget();
-        _shot.calculateShot();
-        _integerChecker.isInt(angle, velocity);
-
-        return "";
+        boolean result = _integerChecker.isInt(angle, velocity);
+        if (result) {
+            _shot.calculateShot(Integer.parseInt(angle), Integer.parseInt(velocity));
+        }
+        return "Please enter valid values for angle and velocity.";
     }
 
 }
