d22b297ca4a01c39ce17cb5cffc7729b1e876b8a
ChrisDwyer <chris.dwyer@outlook.com>
2021-08-02 10:12:59 +0100

    Implemented calculate shot method

--- src/main/java/Shot.java
+++ src/main/java/Shot.java
@@ -1,8 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Shot implements IShot {
 
     @Override
-    public String calculateShot(int angle, int velocity) {
-        return "fire!";
+    public int[] calculateShot(int inputAngle, int inputVelocity) {
+        int xShot = (int) (inputVelocity * (Math.cos(inputAngle)));
+        int yShot = (int) (inputVelocity * (Math.sin(inputAngle)));
+
+        int [] shot = new int [2];
+        shot[0] = Math.round(xShot);
+        shot[1] = Math.round(yShot);
+
+        return shot;
     }
 
 }
