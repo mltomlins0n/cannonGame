3e865b3ecbbe72388dac92d8f10ba2990213aeb6
mltomlins0n <martin070891@gmail.com>
2021-08-02 13:06:28 +0100

    Refactor

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,9 +1,7 @@
 public class InputValidator implements IInputValidator {
     public boolean validateAngleAndVelocityInput(int inputAngle, int inputVelocity) {
-        if (inputAngle >= 1 || inputAngle <= 90) {
-            if (inputVelocity >= 1 || inputVelocity <= 20) {
-                return true;
-            }
+        if ((inputAngle >= 1 && inputAngle <= 90) && (inputVelocity >= 1 && inputVelocity <= 20)) {
+            return true;
         }
         return false;
     }
