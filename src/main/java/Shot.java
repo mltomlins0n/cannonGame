149a8fc39103f555c4754a021956871ac843f1e1
mltomlins0n <martin070891@gmail.com>
2021-08-02 11:43:59 +0100

    Formatting

--- src/main/java/Shot.java
+++ src/main/java/Shot.java
@@ -9,7 +9,7 @@ public class Shot implements IShot {
         int xShot = (int) (inputVelocity * (Math.cos(inputAngle)));
         int yShot = (int) (inputVelocity * (Math.sin(inputAngle)));
 
-        int [] shot = new int [2];
+        int[] shot = new int [2];
         shot[0] = Math.round(xShot);
         shot[1] = Math.round(yShot);
 
