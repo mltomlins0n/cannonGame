920cf1b4a1cdeba883823279fb67b10621d405d9
ChrisDwyer <chris.dwyer@outlook.com>
2021-08-02 10:07:46 +0100

    Added logic to flow

--- src/main/java/CannonGameFlow.java
+++ src/main/java/CannonGameFlow.java
@@ -3,18 +3,24 @@ public class CannonGameFlow implements ICannonGameFlow {
     private ITargetGenerator _targetGenerator;
     private IShot _shot;
     private IIntegerChecker _integerChecker;
+    private IJudge _judgeShot;
+    private IInputValidator _inputValidator;
 
-    public CannonGameFlow(ITargetGenerator targetGenerator, IShot shot, IIntegerChecker integerChecker) {
-        this._targetGenerator = targetGenerator;
-        this._shot = shot;
-        this._integerChecker = integerChecker;
+    public CannonGameFlow(ITargetGenerator _targetGenerator, IShot _shot, IIntegerChecker _integerChecker, IJudge _judgeShot, IInputValidator _inputValidator) {
+        this._targetGenerator = _targetGenerator;
+        this._shot = _shot;
+        this._integerChecker = _integerChecker;
+        this._judgeShot = _judgeShot;
+        this._inputValidator = _inputValidator;
     }
 
     public String flow(String angle, String velocity) {
-        _targetGenerator.getTarget();
+        int[] target = _targetGenerator.getTarget();
         boolean result = _integerChecker.isInt(angle, velocity);
         if (result) {
-            _shot.calculateShot(Integer.parseInt(angle), Integer.parseInt(velocity));
+            int[] shot = _shot.calculateShot(Integer.parseInt(angle), Integer.parseInt(velocity));
+
+            boolean judgeRes = _judgeShot.doesTheShotMatchTheTarget(target,shot)
         }
         return "Please enter valid values for angle and velocity.";
     }
