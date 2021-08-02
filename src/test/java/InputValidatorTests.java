180edbdbe0946bbe75bc1b6e3d33452e7d8ba689
mltomlins0n <martin070891@gmail.com>
2021-08-02 13:07:05 +0100

    Add tests to check whether input is valid

--- src/test/java/InputValidatorTests.java
+++ src/test/java/InputValidatorTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -16,5 +17,42 @@ public class InputValidatorTests {
         boolean result = mockInputValidator.validateAngleAndVelocityInput(inputAngle, inputVelocity);
         // then the output is true as 1 is in bounds
         assertTrue(result);
+    }
+
+    @Test
+    public void AreMaxValuesValid() {
+        // given I am a user
+        int inputAngle = 90;
+        int inputVelocity = 20;
+        // when I input and angle of 90 and a velocity of 20
+        IInputValidator mockInputValidator = mock(IInputValidator.class);
+        boolean result = mockInputValidator.validateAngleAndVelocityInput(inputAngle, inputVelocity);
+        // then the output is true as 1 is in bounds
+        assertTrue(result);
+    }
+
+
+    @Test
+    public void is0Valid() {
+        // given I am a user
+        int inputAngle = 0;
+        int inputVelocity = 0;
+        // when I input and angle and a velocity of 0
+        IInputValidator mockInputValidator = mock(IInputValidator.class);
+        boolean result = mockInputValidator.validateAngleAndVelocityInput(inputAngle, inputVelocity);
+        // then the output is false as 0 is out of bounds
+        assertFalse(result);
+    }
+
+    @Test
+    public void AreValuesAboveMaxValid() {
+        // given I am a user
+        int inputAngle = 91;
+        int inputVelocity = 21;
+        // when I input and angle and a velocity of 0
+        IInputValidator mockInputValidator = mock(IInputValidator.class);
+        boolean result = mockInputValidator.validateAngleAndVelocityInput(inputAngle, inputVelocity);
+        // then the output is false as 0 is out of bounds
+        assertFalse(result);
     }
 }
