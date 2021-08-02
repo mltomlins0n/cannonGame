3c160d691990a89fca8f42ce0ca70168b90dc2a3
mltomlins0n <martin070891@gmail.com>
2021-08-02 15:25:54 +0100

    Fix target spawn

--- src/main/java/TargetGenerator.java
+++ src/main/java/TargetGenerator.java
@@ -4,8 +4,8 @@ public class TargetGenerator implements ITargetGenerator {
 
     @Override
     public int[] getTarget() {
-        int randX = (int) (Math.random() * 11) + 1;
-        int randY = (int) (Math.random() * 11) + 1;
+        int randX = (int) (Math.random() * 10) + 1;
+        int randY = (int) (Math.random() * 10) + 1;
 
         int[] target = new int[2];
         target[0] = Math.round(randX);
