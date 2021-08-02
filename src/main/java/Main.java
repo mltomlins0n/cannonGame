a772f43ad07117e095fec7395c965ecfd1697cfb
ChrisDwyer <chris.dwyer@outlook.com>
2021-08-02 10:11:46 +0100

    Main added from FizzBuzz, needs modification

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+        ICannonGameFlow fClass = context.getBean("flowClassConfig",FlowClass.class);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter an velocity ");
+
+        System.out.print("Please enter an angle ");
+
+        while (true) {
+            String input = sc.next();
+            String result = fClass.flow(input);
+            System.out.println(result);
+            if(input.equalsIgnoreCase("quit")){
+                break;
+            }
+        }
+    }
+}
