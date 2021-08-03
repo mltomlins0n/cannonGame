76e2eaf83baa9d744d6e3292e130c5c2b1d76361
mltomlins0n <martin070891@gmail.com>
2021-08-03 13:11:53 +0100

    Add App config file

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,34 @@
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+
+    @Bean(name = "targetGenerator")
+    public TargetGenerator generateTarget(){
+        return new TargetGenerator();
+    }
+    @Bean(name = "inputValidator")
+    public InputValidator validateAngleAndVelocityInput(){
+        return new InputValidator();
+    }
+    @Bean(name = "shot")
+    public Shot calculateShot(){
+        return new Shot();
+    }
+    @Bean(name = "integerChecker")
+    public IntegerChecker isInt(){
+        return new IntegerChecker();
+    }
+    @Bean(name = "judge")
+    public Judge judgeShot(){
+        return new Judge();
+    }
+    @Bean(name = "shotCounter")
+    public ShotCounter shotCounter(){
+        return new ShotCounter();
+    }
+    @Bean(name = "flowClassConfig")
+    public CannonGameFlow flow(){
+        return new CannonGameFlow(shotCounter(), generateTarget(), calculateShot(),
+                isInt(), judgeShot(), validateAngleAndVelocityInput());
+    }
+}
