c19963d152a2c69d9e0f37d90b2169294ba653a9
ChrisDwyer <chris.dwyer@outlook.com>
2021-08-04 10:14:14 +0100

    Changing logic in ShotFlow

--- src/main/java/ShotFlow.java
+++ src/main/java/ShotFlow.java
@@ -2,21 +2,18 @@ public class ShotFlow implements IShotFlow {
 
     private IShot _shot;
     private IInputValidator _inputValidator;
+    private IJudge _judge;
+    private IShotCounter _shotCounter;
 
     public ShotFlow(IShot _shot, IInputValidator _inputValidator){
         this._shot = _shot;
         this._inputValidator = _inputValidator;
+        this._judge = _judge;
+        this._shotCounter = _shotCounter;
     }
 
     @Override
     public boolean flow(String angle, String velocity) {
-        boolean validShot = _inputValidator.validateAngleAndVelocityInput(validAngle, validVelocity);
-
-        if (validShot) {
-            // if a valid input for shot
-            // then compute shot
-            int[] shot = _shot.calculateShot(validAngle, validVelocity);
-        }
-        return true;
+        if ()
     }
 }
