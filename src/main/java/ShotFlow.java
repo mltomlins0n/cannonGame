dcd0f5e5d09a5c7cff83d8c7df0843fba7767aab
mltomlins0n <martin070891@gmail.com>
2021-08-04 11:09:26 +0100

    Add IntegerChecker

--- src/main/java/ShotFlow.java
+++ src/main/java/ShotFlow.java
@@ -4,17 +4,22 @@ public class ShotFlow implements IShotFlow {
     private IInputValidator _inputValidator;
     private IJudge _judge;
     private IShotCounter _shotCounter;
+    private IIntegerChecker _integerChecker;
 
     public ShotFlow(IShot shot, IInputValidator inputValidator,
-                    IJudge judge, IShotCounter shotCounter) {
+                    IJudge judge, IShotCounter shotCounter, IIntegerChecker integerChecker) {
         this._shot = shot;
         this._inputValidator = inputValidator;
         this._judge = judge;
         this._shotCounter = shotCounter;
+        this._integerChecker = integerChecker;
     }
 
     @Override
     public boolean flow(String angle, String velocity) {
+        int validAngle = Integer.parseInt(angle);
+        int validVelocity = Integer.parseInt(velocity);
+        _shot.calculateShot(validAngle, validVelocity);
         return true;
     }
 }
