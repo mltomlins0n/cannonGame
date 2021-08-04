98420726269eb15140beddd4825aba887531e866
ChrisDwyer <chris.dwyer@outlook.com>
2021-08-04 18:27:34 +0100

    Refactored ShotFlow

--- src/main/java/ShotFlow.java
+++ src/main/java/ShotFlow.java
@@ -3,50 +3,38 @@ import java.util.Arrays;
 public class ShotFlow implements IShotFlow {
 
     private IShot _shot;
-    private IInputValidator _inputValidator;
     private IJudge _judge;
     private IShotCounter _shotCounter;
-    private IIntegerChecker _integerChecker;
 
-    public ShotFlow(IShot shot, IInputValidator inputValidator,
-                    IJudge judge, IShotCounter shotCounter,
-                    IIntegerChecker integerChecker) {
+    public ShotFlow(IShot shot, IJudge judge, IShotCounter shotCounter) {
+
         this._shot = shot;
-        this._inputValidator = inputValidator;
         this._judge = judge;
         this._shotCounter = shotCounter;
-        this._integerChecker = integerChecker;
+
     }
 
     @Override
-    public boolean flow(String angle, String velocity, int[] target) {
+    public boolean shotFlow(int angle, int velocity, int[] target) {
         boolean targetHit = false;
 
-        boolean validInput = _integerChecker.isInt(angle, velocity);
+        int[] shot = _shot.calculateShot(angle, velocity);
+        _shotCounter.incrementCounter();
+        targetHit = _judge.judgeShot(shot, target);
+        System.out.println("The target is at " + Arrays.toString(target) +
+                ", your shot landed at " + Arrays.toString(shot));
 
-        if (validInput) {
-            int validAngle = Integer.parseInt(angle);
-            int validVelocity = Integer.parseInt(velocity);
-            boolean validShot = _inputValidator.validateAngleAndVelocityInput(validAngle, validVelocity);
-
-            if (validShot) {
-                int[] shot =_shot.calculateShot(validAngle, validVelocity);
-                _shotCounter.incrementCounter();
-                targetHit = _judge.judgeShot(shot, target);
-                System.out.println("The target is at " + Arrays.toString(target) +
-                        ", your shot landed at " + Arrays.toString(shot));
-
-                if (targetHit) {
-                    System.out.print("You've hit your target!!! Yeeeaaaaaahh!");
-                    _shotCounter.incrementCounter();
-                    System.out.println("\nIt took you " + _shotCounter.getCounter() + " shots.");
-                } else {
-                    System.out.print("You missed, please enter another shot");
-                }
-            } else {
-                System.out.println("Please enter valid values for angle and velocity.");
-            }
+        if (targetHit) {
+            System.out.print("You've hit your target!!! Yeeeaaaaaahh!");
+            _shotCounter.incrementCounter();
+            System.out.println("\nIt took you " + _shotCounter.getCounter() + " shots.");
+        } else {
+            System.out.print("You missed, please enter another shot");
         }
         return targetHit;
     }
 }
+
+
+
+
