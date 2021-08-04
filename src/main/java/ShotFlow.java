1a31d26fcc9dc04d1c856ea6bc1c64d4a5d317e6
mltomlins0n <martin070891@gmail.com>
2021-08-04 10:50:10 +0100

    Refactor variable names

--- src/main/java/ShotFlow.java
+++ src/main/java/ShotFlow.java
@@ -5,15 +5,16 @@ public class ShotFlow implements IShotFlow {
     private IJudge _judge;
     private IShotCounter _shotCounter;
 
-    public ShotFlow(IShot _shot, IInputValidator _inputValidator){
-        this._shot = _shot;
-        this._inputValidator = _inputValidator;
-        this._judge = _judge;
-        this._shotCounter = _shotCounter;
+    public ShotFlow(IShot shot, IInputValidator inputValidator,
+                    IJudge judge, IShotCounter shotCounter) {
+        this._shot = shot;
+        this._inputValidator = inputValidator;
+        this._judge = judge;
+        this._shotCounter = shotCounter;
     }
 
     @Override
     public boolean flow(String angle, String velocity) {
-        if ()
+        return true;
     }
 }
