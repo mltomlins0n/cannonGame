4a2f5a1fc6fb5539a0f6417f9a6aaec901d173b7
mltomlins0n <martin070891@gmail.com>
2021-08-06 13:31:03 +0100

    Add logger to suppress mongoDB console output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger rootLogger = loggerContext.getLogger("org.mongodb.driver");
+        rootLogger.setLevel(Level.ERROR);
+
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         IRoundFlow roundFlow = applicationContext.getBean("roundFlow", RoundFlow.class);
-        
+
         roundFlow.roundFlow("1", "1");
     }
-
 }
