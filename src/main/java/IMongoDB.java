75e166642bc5e64dca149330c418b8c78adb7ec3
mltomlins0n <martin070891@gmail.com>
2021-08-06 13:33:12 +0100

    Add MongoDB class

---
+++ src/main/java/IMongoDB.java
@@ -0,0 +1,7 @@
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
+
+public interface IMongoDB {
+
+    void displayLeaderboard();
+}
---
+++ src/main/java/MongoDB.java
@@ -0,0 +1,39 @@
+import com.mongodb.client.*;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+public class MongoDB implements IMongoDB {
+
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection<Document> collection;
+    String uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+
+    public MongoDB() {
+        this.mongoClient = MongoClients.create(uri);
+        this.database = mongoClient.getDatabase("playerData");
+        this.collection = database.getCollection("players");
+    }
+
+    public void displayLeaderboard() {
+
+        Bson projectionFields = Projections.fields(
+                Projections.include("name", "score"),
+                Projections.excludeId());
+
+        MongoCursor<Document> results = collection.find()
+                .projection(projectionFields)
+                .sort(Sorts.descending("name", "score")).iterator();
+
+        System.out.println("Leaderboard\n-------------");
+        try {
+            while(results.hasNext()) {
+                System.out.println(results.next().toJson());
+            }
+        } finally {
+            results.close();
+        }
+    }
+}
